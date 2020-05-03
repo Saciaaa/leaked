4c832ba700cb7d48372ab5c794cf5c181349886d
Saciaaa <64750340+Saciaaa@users.noreply.github.com>
2020-05-03 21:49:59 +0200

    info

--- Leaker.py
+++ Leaker.py
@@ -2,15 +2,42 @@ from os import system
 import os
 import sys
 import time
-info = input("Paste info here!:   ")
+
+
+BLUE = '\33[94m'
+LightBlue = '\033[94m'
+RED = '\033[91m'
+WHITE = '\33[97m'
+YELLOW = '\33[93m'
+GREEN = '\033[32m'
+LightCyan    = "\033[96m"
+END = '\033[0m'
+
+if len(sys.argv) < 2:
+    os.system("clear || cls")
+    sys.stdout.write(BLUE + """  
+███████╗ █████╗  ██████╗██╗ █████╗     ██╗███╗   ██╗███████╗ ██████╗     ██╗     ███████╗ █████╗ ██╗  ██╗███████╗██████╗ 
+██╔════╝██╔══██╗██╔════╝██║██╔══██╗    ██║████╗  ██║██╔════╝██╔═══██╗    ██║     ██╔════╝██╔══██╗██║ ██╔╝██╔════╝██╔══██╗
+███████╗███████║██║     ██║███████║    ██║██╔██╗ ██║█████╗  ██║   ██║    ██║     █████╗  ███████║█████╔╝ █████╗  ██████╔╝
+╚════██║██╔══██║██║     ██║██╔══██║    ██║██║╚██╗██║██╔══╝  ██║   ██║    ██║     ██╔══╝  ██╔══██║██╔═██╗ ██╔══╝  ██╔══██╗
+███████║██║  ██║╚██████╗██║██║  ██║    ██║██║ ╚████║██║     ╚██████╔╝    ███████╗███████╗██║  ██║██║  ██╗███████╗██║  ██║
+╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝╚═╝  ╚═╝    ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝     ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                                                                       
+             
+                                                                                          
+    """  + END+BLUE+'Sacia Info Leaker'.format(BLUE, END).center(69) +
+    '\n' + '\tMade by: {}Sacia'.format(BLUE, BLUE).center(76) +
+'\n' + '\tVersion: {}2.0{}\n'.format(BLUE, END).center(80) + '\n')
+
+info = input(BLUE+"Paste info here!:   ")
 text = info
 file = open('info.txt', 'w')
 file.write(text)
 file.close()
-print("leaking info to database!")
+print(RED+"leaking info to database!")
 
 system('git add .')
 system('git commit -m "info"')
 system('git push -f origin master')
-print("Info Is now public!")
+print(RED+"Info Is now public!")
 
